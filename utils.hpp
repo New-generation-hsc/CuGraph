284f237dd161fb0df0c924e2ae55645590a9a9d1
New-generation-hsc <hsc199761@163.com>
2019-04-24 10:43:37 +0800

    modify write to file function argument to filename

--- utils.hpp
+++ utils.hpp
@@ -46,7 +46,10 @@ FILE *open_file_access(const char*, const char*);
  * @param[count] the number of elements to write
 */
 template<typename Value>
-void write_to_file(FILE *fp, Value *ptr, const uint *labels, int count){
+void write_to_file(const char* filename, Value *ptr, const uint *labels, int count){
+	
+	FILE *fp = open_file_access(filename, "w");
+
 	for(uint idx = 0; idx < count; idx++){
 		fprintf(fp, "%u -> ", labels[idx]);
 		fprintf(fp, template_print<Value>::fmt, ptr[idx]);
